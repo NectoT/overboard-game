7b1aa1b7daebd4beb83bf6a19f6e11d704989528
NectoT <nectot2@gmail.com>
2023-09-06 14:46:02 +0300

    Из SupplyCard извлечён базовый компонент GameCard

---
+++ frontend/src/routes/[id]/GameCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let hoverable = true;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- TODO: возможность выбора карты с помощью клавиатуры -->
+<div class="card" class:hoverable
+on:click
+>
+<slot></slot>
+</div>
+
+<style>
+    .card {
+        width: var(--width, 100%);
+        aspect-ratio: 2.2 / 3;
+
+        background-image: var(--background-image);
+        background-size: cover;
+
+        transition: transform 0.5s;
+
+        transform: rotate(var(--rotation, 0));
+        transform-origin: var(--transform-origin, center center);
+    }
+
+    .hoverable {
+        transition: transform 0.5s, z-index 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+        z-index: 1;
+    }
+
+    .hoverable:hover {
+        transform: scale(var(--hoverScale, 1));
+        z-index: 1000;
+    }
+</style>
--- frontend/src/routes/[id]/SupplyCard.svelte
+++ frontend/src/routes/[id]/SupplyCard.svelte
@@ -1,39 +1,10 @@
 <script lang="ts">
-    // Если что это не уверсальная карта припасов, пока что
+    import GameCard from "./GameCard.svelte";
+
     export let type: string;
 
     export let hoverable = true;
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<!-- TODO: возможность выбора карты с помощью клавиатуры -->
-<div class="card" class:hoverable
-style:background-image='url("supplies/flaregun.png")'
-on:click
->
-</div>
-
-<style>
-    .card {
-        width: var(--width, 100%);
-        aspect-ratio: 2.2 / 3;
-
-        background-size: cover;
-
-        transition: transform 0.5s;
-
-        transform: rotate(var(--rotation, 0));
-        transform-origin: var(--transform-origin, center center);
-    }
-
-    .hoverable {
-        transition: transform 0.5s, z-index 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
-        z-index: 1;
-    }
-
-    .hoverable:hover {
-        transform: scale(var(--hoverScale, 1));
-        z-index: 1000;
-    }
-</style>
+<GameCard --background-image='url("supplies/flaregun.png")' hoverable={hoverable} on:click>
+</GameCard>
